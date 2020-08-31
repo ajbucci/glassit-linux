0f678b26e840753bcebadb6e6649c0bffe6ae8c6
ajbucci <ajbucci@gmail.com>
2020-08-31 18:50:45 -0400

    update extension.js
    Added support for OSS and vanilla VS Code selection through extension settings

--- extension.js
+++ extension.js
@@ -6,7 +6,13 @@ function activate(context) {
     if (!opacity) {
         opacity = 97;
     }
-    const cmd = `bash -c 'for W in $(wmctrl -l |grep "Visual Studio Code" |awk '"'"'{print $1}'"'"'); do xprop -id $W -format _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $(printf 0x%x $((0xffffffff * ${opacity} / 100))); done'`;
+    let codename = vscode.workspace.getConfiguration("glassit-linux").get("codename");
+    if (codename === "CodeOSS") {
+        codename = "Code - OSS";
+    } else {
+        codename = "Visual Studio Code";
+    }
+    const cmd = `bash -c 'for W in $(wmctrl -l |grep "${codename}" |awk '"'"'{print $1}'"'"'); do xprop -id $W -format _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $(printf 0x%x $((0xffffffff * ${opacity} / 100))); done'`;
     const terminal = vscode.window.createTerminal('glassit-linux');
     terminal.sendText(cmd);
     terminal.sendText(`exit`);
